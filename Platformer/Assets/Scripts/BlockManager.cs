56f1d7252187b2ee3876b0bb1179368745d7582d
Uzair <uzairmahmed@gmail.com>
2019-12-30 21:36:10 -0500

    BlockSpawning
    still buggy

---
+++ Platformer/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    GameObject anchors;
+    GameObject block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anchors = transform.Find("Anchors").gameObject;
+        block = transform.Find("Block").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Platformer/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject blockPrefab;
+
+    public float timer;
+    public float waitTime;
+
+    public float offsetX, offsetY, offsetZ;
+
+    public float targetOffsetX, targetOffsetY, targetOffsetZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        transform.position = target.transform.position + new Vector3(offsetX, offsetY, offsetZ);
+
+        if (timer >= waitTime)
+        {
+            Vector3 spawnTarget = target.transform.position + new Vector3(targetOffsetX, targetOffsetY, targetOffsetZ);
+            GameObject blk = Instantiate(blockPrefab, transform.position, Quaternion.identity);
+
+            blk.transform.Find("Anchors").gameObject.transform.position = spawnTarget;
+        }
+    }
+}
